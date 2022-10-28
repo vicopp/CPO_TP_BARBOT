6855d917eb92050132f764bc4d293929d0193c71
victor <victor@10.3.61.103>
2022-10-28 15:28:44 +0200

    Création du projet TP2

--- TP2_Bieres_BARBOT/src/tp2_bieres_barbot/TP2_Bieres_BARBOT.java
+++ TP2_Bieres_BARBOT/src/tp2_bieres_barbot/TP2_Bieres_BARBOT.java
@@ -14,16 +14,17 @@ public class TP2_Bieres_BARBOT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
         uneBiere.nom = "Cuvée des trolls"; uneBiere.degreAlcool = 7.0 ;
         uneBiere.Brasserie = "Dubuisson";
         uneBiere.Ouvert = false;
         uneBiere.lireEtiquette();
-        BouteilleBiere autreBiere = new BouteilleBiere();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         autreBiere.nom = "Leffe"; autreBiere.degreAlcool = 6.6;
         autreBiere.Brasserie = "Abbaye de Leffe";
         uneBiere.Ouvert = false;
         uneBiere.lireEtiquette();
+        
                 
                 
              
