7b4107e7e9e2f6d1abd1265c411e7414610b7804
victor <victor@10.3.61.103>
2022-10-29 18:56:33 +0200

    conversions depuis tp1 + fonction @overrid

--- TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/Convertisseur.java
+++ TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/Convertisseur.java
@@ -8,10 +8,81 @@ package tp2_convertisseurobjet_barbot;
  *
  * @author victor
  */
+
 public class Convertisseur {
-    
+    double nbConversions = 0;
+    @Override
+    public String toString () {
+ return "nb de conversions"+ nbConversions;
+    }
 
 
 
-    
+    public double CelciusVersKelvin(double celcius){
+
+        double Kelvin = celcius + 273.15;
+        nbConversions += 1;
+        return Kelvin;
+
+    }
+
+
+    public double KelvinVersCelcius(double kelvin){
+
+        double celcius = kelvin - 273.15;
+        nbConversions += 1;
+
+        return celcius;
+
+    }
+
+
+    public double FarenheitVersCelcius(double farenheit){
+
+        double celcius = (farenheit - 32) / 1.8;
+        nbConversions += 1;
+
+        return celcius;
+
+    }
+
+
+    public double CelciusVersFarenheit(double celcius){
+
+        double farenheit = (celcius * 1.8) + 32;
+        nbConversions += 1;
+
+        return farenheit;
+
+    }
+
+
+    public double KelvinVersFarenheit(double kelvin){
+
+        double farenheit = CelciusVersFarenheit(KelvinVersCelcius(kelvin));
+        nbConversions += 1;
+
+        return farenheit;
+
+    }
+
+
+    public double FarenheitVersKelvin(double farenheit){
+
+        double kelvin = CelciusVersKelvin(FarenheitVersCelcius(farenheit));
+        nbConversions += 1;
+
+        return kelvin;
+
+    }
+
 }
+   
+        
+        
+   
+
+
+
+    
+
