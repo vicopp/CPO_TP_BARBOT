0d9a8755b243a8710ab827342742c6ca10bc257f
victor <victor@10.3.61.103>
2022-10-29 20:20:41 +0200

    TP2 etape 3

---
+++ TP2_manip_BARBOT/src/tp2_manip_barbot/TP2_manip_BARBOT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_barbot;
+
+/**
+ *
+ * @author victor
+ */
+public class TP2_manip_BARBOT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
