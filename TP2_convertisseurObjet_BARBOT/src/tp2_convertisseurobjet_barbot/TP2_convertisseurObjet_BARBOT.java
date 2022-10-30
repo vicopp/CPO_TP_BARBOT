285118730ae3bcfaa7c7d482a570ecca4150584a
victor <victor@10.3.61.103>
2022-10-30 18:01:15 +0100

    reprise etape 2 tp 2 creation convertisseur

--- TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/TP2_convertisseurObjet_BARBOT.java
+++ TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/TP2_convertisseurObjet_BARBOT.java
@@ -14,44 +14,17 @@ public class TP2_convertisseurObjet_BARBOT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-        
-        Convertisseur FarenheitVersCelcius = new Convertisseur(); //convertisseur farenheit en celcius
-        
-        Convertisseur x;
-        x = FarenheitVersCelcius;
-        
-        Convertisseur CelciusVersFarenheit = new Convertisseur(); // convertisseur celcius en farenheit
-        
-        Convertisseur y;
-        y = CelciusVersFarenheit;
-        
-        Convertisseur KelvinVersFarenheit = new Convertisseur(); // convertisseur kelvin en farenheit
-        
-        Convertisseur z;
-        z = KelvinVersFarenheit;
-        
-        Convertisseur FarenheitVersKelvin = new Convertisseur(); // convertisseur farenheit en kelvin
-        
-        Convertisseur w;
-        w = FarenheitVersKelvin;
-        
-        Convertisseur CelciusVersKelvin = new Convertisseur(); // convertisseur celcius en kelvin
-        
-        Convertisseur a;
-        a = CelciusVersKelvin;
-        
-        Convertisseur KelvinVersCelcius = new Convertisseur(); // convertisseur kelvin en celcius
-        
-        Convertisseur b;
-        b = KelvinVersCelcius;
-                
-        System.out.println (nbConversions);
-        
+        double res;
+        Convertisseur monconvertisseur = new Convertisseur();
         
         
     }
+        
+    }
+        // TODO code application logic here
+        
+        
+     
+
+
     
-}
