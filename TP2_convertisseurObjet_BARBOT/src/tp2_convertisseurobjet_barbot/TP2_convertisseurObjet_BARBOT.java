3b6d7f1eb512d403b6587b5dcf3a33f8ba2907d4
victor <victor@10.3.61.103>
2022-10-30 18:22:28 +0100

    rendu code grace explication ami

--- TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/TP2_convertisseurObjet_BARBOT.java
+++ TP2_convertisseurObjet_BARBOT/src/tp2_convertisseurobjet_barbot/TP2_convertisseurObjet_BARBOT.java
@@ -21,10 +21,59 @@ public class TP2_convertisseurObjet_BARBOT {
        
         System.out.println("Veuillez choisir une conversion 1:(Celcius vers Kelvin) 2:(Kelvin vers Celcius) 3:(Farenheit vers Celcius) 4:(Celcius vers Farenheit) 5:(Kelvin vers Farenheit) 6:(Farenheit vers Kelvin)");
 
-    }
         
-    }
-        // TODO code application logic here
+        
+        int a = sc.nextInt();
+    if (a==1){
+           System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+        res=monconvertisseur.CelciusVersKelvin(a);
+                System.out.println(res);}
+                else if (a==2){
+                    System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+                        res=monconvertisseur.KelvinVersCelcius(b);
+                                System.out.println(res);}
+                else if (a==3){
+                       System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+                        res=monconvertisseur.FarenheitVersCelcius(b);
+                                                        System.out.println(res);}
+                else if (a==4){
+                       System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+                        res=monconvertisseur.CelciusVersFarenheit(b);
+                                                        System.out.println(res);}
+                else if (a==5){
+                       System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+                        res=monconvertisseur.CelciusVersFarenheit(b);
+                                                        System.out.println(res);}
+                else {
+                       System.out.println("Veuillez mettre la valeur a convertir");
+                    int b = sc.nextInt();
+                        res=monconvertisseur.CelciusVersFarenheit(b);
+                                                        System.out.println(res);}
+
+
+
+
+
+
+
+
+}
+   
+                       
+
+                       
+                        }
+   
+   
+    
+        
+    
+     
         
         
      
