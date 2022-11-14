12d01c3c1443213abaf8bcaf0080f490aac3d2c5
victor <victor@10.3.61.103>
2022-11-14 09:57:33 +0100

    debut tp3

---
+++ TP3_Barbot/src/tp3_barbot/TP3_Barbot.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_barbot;
+
+/**
+ *
+ * @author victor
+ */
+public class TP3_Barbot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Arme m = new Arme("arme standart", 4);
+    
+    m.setAttaque(25);
+    
+    int i = m.getAttaque()
+    
+    
+    
+    }
+
+    public TP3_Barbot() {
+    }
+    
+}
