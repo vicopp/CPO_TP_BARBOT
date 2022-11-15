aec2234b197562d2f42caa4a066e552121a935cc
victor <victor@10.3.61.103>
2022-11-15 14:58:14 +0100

    creation guerrier magicien avec booleen

---
+++ TP3_Barbot/src/Guerrier.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author victor
+ */
+public class Guerrier extends Personnages {
+    
+    boolean c;
+    
+    public Guerrier(String n, int i, boolean cheval) {
+        super(n, i);
+        c=cheval;
+    }
+    
+}
