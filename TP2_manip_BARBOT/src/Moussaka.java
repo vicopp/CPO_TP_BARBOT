ae42d7dcf12d0a0ab727fd8d1b6cbf3a731a144d
victor <victor@10.3.61.103>
2022-12-26 15:32:40 +0100

    commit debut projet

---
+++ TP2_manip_BARBOT/src/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author victor
+ */
+public class Moussaka {
+    int nbCalories;
+    
+    
+   public Moussaka(int nbCal) {
+       nbCalories = nbCal;
+   }
+    
+}
---
+++ TP2_manip_BARBOT/src/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author victor
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    
+   public Tartiflette(int nbCal) {
+       nbCalories = nbCal;
+   }
+    
+}
