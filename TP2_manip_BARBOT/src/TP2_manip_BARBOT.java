41f69d001d8ae41f8f983ae6ef4695659f4a217d
victor <victor@10.3.61.103>
2022-12-26 15:35:11 +0100

    commit fin projet

--- TP2_manip_BARBOT/src/TP2_manip_BARBOT.java
+++ TP2_manip_BARBOT/src/TP2_manip_BARBOT.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tp2_manip_barbot;
+
 
 /**
  *
@@ -16,9 +16,9 @@ public class TP2_manip_BARBOT {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Tartiflette assiette1 = new Tartiflette(500) ; 
-        Tartiflette assiette2 = new Tartiflette(600) ;
-        Tartiflette assiette3 = assiette2;             // seulement 2 tartiflettes créées
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
+        Tartiflette assiette3 = assiette2;            // seulement 2 tartiflettes créées
                                                        // assiette 2 et 3 font reference a la meme assiette
        
         
